6d067b51954973d29b3cc9b218436b3f22d50bd4
Edward Parada <edwardpara910@gmail.com>
2022-11-01 12:35:48 -0500

    Added POST

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const express = require('express');
 const redis = require('redis');
 
@@ -14,6 +16,8 @@ const client = redis.createClient({
 client.on('error', err => {
     console.log('Redis error: ' + err);
 });
+
+client.connect();
 
 console.log(`Conectado a redis en ${redis_host}:${redis_port}`)
 
@@ -36,19 +40,26 @@ app.get('/students', (req, res) => {
     })
 });
 
-app.post('/addstudent/:codigo', (req, res) => {
-    const { codigo } = req.params;
+app.post('/addstudent', (req, res) => {
+    const { code } = req.body;
     const { name } = req.body;
     const { program } = req.body;
     const { email } = req.body;
     const { level } = req.body;
 
-
-    if (!codigo) {
+    if (!code) {
         res.status(418).send({ message: 'Complete la información!'})
+        return
     }
 
+    client.hSet(toString(code), req.body)
 
+    const printClient = async () => {
+        const a = await client.hGetAll(toString(code));
+        console.log(a);
+    };
+
+    printClient();
 
     res.send({
         newStudent: `Agregado el estudiante ${name} con código ${code} de nivel ${level} del programa de ${program}`
